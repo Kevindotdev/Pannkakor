4a8dad9536f72d9ee0e1539ac6b8d4b09d33b686
Kevin <n/a>
2026-05-29 18:07:40 +0200

    feat: improved mobile navbar interaction

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const burgerMenu = document.querySelector(".burgerMenu");
 const navbar = document.querySelector(".navbar__links");
+const overlay = document.querySelector(".navbar__overlay");
 
 burgerMenu.addEventListener("click", () => {
   navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
+overlay.addEventListener("click", () => {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
 });
